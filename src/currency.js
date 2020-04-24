02af9cd6f1531485b8df34dd7a0492e7f04abece
Steven Ly <1stevenly92@gmail.com>
2020-04-24 12:43:23 -0400

    able to access api but its not pulling up the right data

--- src/currency.js
+++ src/currency.js
@@ -1,12 +1,12 @@
 // import $ from 'jquery';
 
 
-export class Currency {
+export class CurrencyService {
   async getCurrency(){
-  try {
-    let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
-
-  let jsonifiedResponse;
+   try {
+  //   let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+  let response = await fetch(`https://prime.exchangerate-api.com/v5/17c5f156ef4ab5c5d16bf484/latest/USD`);
+    let jsonifiedResponse;
   if (response.ok && response.status === 200) {
     jsonifiedResponse = await response.json();
   } else {
@@ -15,16 +15,6 @@ export class Currency {
   return jsonifiedResponse;
 } catch(error) {
   return false;
-}
-}
-
-
-
-
-
-
-
-
-
-
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,34 +1,26 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-import { Currency } from './currency';
-import $ from 'jquery';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import { CurrencyService } from "./currency";
+import $ from "jquery";
 
-
-
-
-
-$(document).ready(function(){
-    $("#submit").click(function(event){
-        event.preventDefault();
-        const usd = $("#num-input").val();
-        console.log(usd);
-    
+$(document).ready(function () {
+  $("#submit").click(function (event) {
+    event.preventDefault();
+    const usd = $("#num-input").val();
+    console.log(usd);
+  
 
     (async () => {
-        let currency = new Currency();
-        const response = await currency.getCurrency();
-        getElements(response);
-        
-        function getElements(response){
-            if(response){
-                $(".aud").text(`${response.conversion_rate.AUD} * usd`);
-
-            }
-        }
-    })
-
-
-})
-
-})
+      let currencyService = new CurrencyService();
+      const response = await currencyService.getCurrency();
+      getElements(response);
+    })();
+    function getElements(response) {
+      if (response) {
+        $(".aud").text(`This is something ${response.conversion_rates.AED} Yes`);
+        console.log(`${response.conversion_rates.AED}`)
+      }
+    }
+  });
+});
