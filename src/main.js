e0e3c84584a2de88b0253fa59431b7acbd5600cb
Steven Ly <1stevenly92@gmail.com>
2020-04-24 16:10:12 -0400

    added some handle errors

--- src/main.js
+++ src/main.js
@@ -44,6 +44,13 @@ $(document).ready(function () {
             $("#display").text( `$1 = ${response.conversion_rates.CAD} AUD. Converting ${usd} dollars to AUD. You would have ${parseFloat(usd * response.conversion_rates.AUD.toFixed(2))} AUD`);
             $("#error").hide();
             $("#warning").hide();
+          }if (response.result = undefined){
+              $("#error").text("Something is wrong with URL");
+              $("#warning").hide();
+
+          }if (response.result == "error"){
+              $("#error").text("Something is wrong with api key");
+
           }
           else{
             $("#error").text("Please input one of the 5 currencies we support; AUD, CAD, DOP, HKD, SEK");
