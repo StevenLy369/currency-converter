dc0e45715f998a04579e315e334a23d73ac96db6
Steven Ly <1stevenly92@gmail.com>
2020-05-03 11:20:30 -0700

    fixed error with adding a new env, wasnt in the right spot

--- src/currency.js
+++ src/currency.js
@@ -4,7 +4,7 @@
 export class CurrencyService {
   async getCurrency(){
    try {
-   let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+    let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
    //let response = await fetch(`https://prime.exchangerate-api.com/v5/17c5f156ef4ab5c5d16bf484/latest/USD`);
     let jsonifiedResponse;
   if (response.ok && response.status === 200) {
--- src/main.js
+++ src/main.js
@@ -20,38 +20,41 @@ $(document).ready(function () {
           getElements(response);
 
         function getElements(response) {
+        if (response.result === undefined){
+            $("#error").text("Something is wrong with URL");
+            $("#warning").hide();
+            console.log("hello");
+
+        }
+        else if (response.result === "error"){
+            $("#error").html(`<p>Something is wrong with api key</p>`);
+            $("#warning").hide();
+            console.log("bye");
+        }
           if (convert == "CAD") {
             $("#display").text( `$1 = ${response.conversion_rates.CAD} CAD. Converting ${usd} dollars to CAD. You would have ${parseFloat(usd * response.conversion_rates.CAD.toFixed(2))} CAD`);
             $("#error").hide();
             $("#warning").hide();
           }
-          if (convert == "HKD") {
+          else if (convert == "HKD") {
             $("#display").text( `$1 = ${response.conversion_rates.HKD} HKD. Converting ${usd} dollars to HKD. You would have ${parseFloat(usd * response.conversion_rates.HKD.toFixed(2))} HKD`);
             $("#error").hide();
             $("#warning").hide();
           }
-          if (convert == "DOP") {
+          else if (convert == "DOP") {
             $("#display").text( `$1 = ${response.conversion_rates.DOP} DOP. Converting ${usd} dollars to DOP. You would have ${parseFloat(usd * response.conversion_rates.DOP.toFixed(2))} DOP`);
             $("#error").hide();
             $("#warning").hide();
           }
-          if (convert == "SEK") {
+          else if (convert == "SEK") {
             $("#display").text( `$1 = ${response.conversion_rates.SEK} sek. Converting ${usd} dollars to SEK. You would have ${parseFloat(usd * response.conversion_rates.SEK.toFixed(2))} SEK`);
             $("#error").hide();
             $("#warning").hide();
           }
-          if (convert == "AUD") {
+          else if (convert == "AUD") {
             $("#display").text( `$1 = ${response.conversion_rates.CAD} AUD. Converting ${usd} dollars to AUD. You would have ${parseFloat(usd * response.conversion_rates.AUD.toFixed(2))} AUD`);
             $("#error").hide();
             $("#warning").hide();
-          }if (response.result == undefined){
-              $("#error").text("Something is wrong with URL");
-              $("#warning").hide();
-
-          }if (response.result === "error"){
-              $("#error").text("Something is wrong with api key");
-              $("#warning").hide();
-
           }
           else{
             $("#error").text("Please input one of the 5 currencies we support; AUD, CAD, DOP, HKD, SEK");
